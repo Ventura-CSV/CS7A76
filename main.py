3d552a0f1bb2dbfb09f9abde47a5e341777e6ec3
Kyu Lee <kwooler@gmail.com>
2023-05-30 00:03:49 -0700

    A76

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-def main():
-    num1 = list(map(int, input().split()))
-    num2 = list(map(int, input().split()))
-    ##################################################
-    # Code your program here
-    ##################################################
-    main.merged = []
+def mergeList(num1, num2):
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
 
-##
+
+def main():
+    num1 = [1, 3, 5, 7]
+    num2 = [2, 4, 6, 8]
+    merged = mergeList(num1, num2)
+    print(f'The merged list is {merged}')
+
+    num1 = [1, 2, 3, 4]
+    num2 = [5, 6, 7, 8]
+    merged = mergeList(num1, num2)
+    print(f'The merged list is {merged}')
+
+    num1 = [5, 10, 25, 75, 85]
+    num2 = [45, 55, 60]
+    merged = mergeList(num1, num2)
+    print(f'The merged list is {merged}')
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -8,52 +8,32 @@ import math
 def test_main_1():
     captureout = io.StringIO()
     sys.stdout = captureout
-    datastr = '1 2 3\n4 5 6 7 8'
-    sys.stdin = io.StringIO(datastr)
+    # datastr = '1 2 3\n4 5 6 7 8'
+    # sys.stdin = io.StringIO(datastr)
 
-    main.main()
     sys.stdout = sys.__stdout__
     print('captured ', captureout.getvalue())
     lines = captureout.getvalue().split('\n')
     print(lines)
 
-    # regex_string = r'[\w,\w]*1'
-    # regex_string += r'[\w,\w]*3'
-    # regex_string += r'[\w,\w]*5'
-    # regex_string += r'[\w,\w]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != none
-    # print(res.group())
-    assert len(main.main.merged) == 8
-    assert main.main.merged[0] == 1
-    assert main.main.merged[2] == 3
-    assert main.main.merged[6] == 7
-    assert main.main.merged[7] == 8
+    num1 = [1, 3, 5, 7]
+    num2 = [2, 4, 6, 8]
+    merged = main.mergeList(num1, num2)
+    print(f'The merged list is {merged}')
+    assert merged == [1, 2, 3, 4, 5, 6, 7, 8]
 
 
 def test_main_2():
-    captureout = io.StringIO()
-    sys.stdout = captureout
-    datastr = '10 5 25 75 85\n55 60 45'
-    sys.stdin = io.StringIO(datastr)
+    num1 = [1, 2, 3, 4]
+    num2 = [5, 6, 7, 8]
+    merged = main.mergeList(num1, num2)
+    print(f'The merged list is {merged}')
+    assert merged == [1, 2, 3, 4, 5, 6, 7, 8]
 
-    main.main()
-    sys.stdout = sys.__stdout__
-    print('captured ', captureout.getvalue())
-    lines = captureout.getvalue().split('\n')
-    print(lines)
 
-    # regex_string = r'[\w,\w]*1'
-    # regex_string += r'[\w,\w]*3'
-    # regex_string += r'[\w,\w]*5'
-    # regex_string += r'[\w,\w]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != none
-    # print(res.group())
-    assert len(main.main.merged) == 8
-    assert main.main.merged[0] == 55
-    assert main.main.merged[2] == 45
-    assert main.main.merged[6] == 75
-    assert main.main.merged[7] == 85
+def test_main_3():
+    num1 = [5, 10, 25, 75, 85]
+    num2 = [45, 55, 60]
+    merged = main.mergeList(num1, num2)
+    print(f'The merged list is {merged}')
+    assert merged == [5, 10, 25, 45, 55, 60, 75, 85]
